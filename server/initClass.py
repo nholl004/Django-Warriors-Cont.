0856266008f9a8960a8ed85c1c862d5287be2eba
nholl004 <nholl004@ucr.edu>
2021-11-28 17:17:14 -0800

    removing globals incorerating class system

---
+++ server/initClass.py
@@ -0,0 +1,11 @@
+class initList:
+    def __init__(self):
+        self.list = [[]]
+class init_Top10:
+    def __init__(self):
+        self.listC = []
+        self.listD = []
+        self.listR = []
+        self.sumC = 0
+        self.sumD = 0
+        self.sumR = 0
